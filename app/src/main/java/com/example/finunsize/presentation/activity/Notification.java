89f8fb7e504c81056d39351f1961693f9fa68235
NMV <nicolas.viana2@etec.sp.gov.br>
2023-10-27 11:32:07 -0300

    Att Notification e Config

---
+++ app/src/main/java/com/example/finunsize/presentation/activity/Notification.java
@@ -0,0 +1,17 @@
+package com.example.finunsize.presentation.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.finunsize.R;
+
+
+public class Notification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
